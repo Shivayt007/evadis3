c1367af9e185764b56071ade9b1c3fb81a988414
Race Developer Members <88087547+Shivayt007@users.noreply.github.com>
2023-07-07 20:41:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1602,7 +1602,7 @@ async def auto_filter(client, msg, spoll=False):
         
     
     btn.insert(0, [
-        InlineKeyboardButton("⚡ Cʜᴇᴄᴋ Bᴏᴛ PM ⚡", url=f"https://t.me/{temp.U_NAME}")
+        InlineKeyboardButton("⚡ How To Download ⚡", url=f"https://t.me/tgtamillinks/2")
     ])
     btn.insert(0, [
         InlineKeyboardButton("⚡ Cʜᴇᴄᴋ Bᴏᴛ PM ⚡", url=f"https://t.me/{temp.U_NAME}")
