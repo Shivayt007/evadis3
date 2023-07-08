d7fdc4b7e429604fad137e6f4a63e5169e042c8d
Race Developer Members <88087547+Shivayt007@users.noreply.github.com>
2023-07-08 22:28:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1708,7 +1708,7 @@ async def auto_filter(client, msg, spoll=False):
                 await fek.delete()
                 await message.delete()
     else:
-        fuk = await message.reply_message(text=cap, reply_markup=InlineKeyboardMarkup(btn))
+        fuk = await message.reply_texr(text=cap, reply_markup=InlineKeyboardMarkup(btn))
         try:
             if settings['auto_delete']:
                 await asyncio.sleep(600)
@@ -1743,7 +1743,7 @@ async def advantage_spell_chok(client, msg):
         ]]
         if NO_RESULTS_MSG:
             await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
-        k = await msg.reply_message(
+        k = await msg.reply_texf(
              
             text=script.I_CUDNT.format(mv_rqst),
             reply_markup=InlineKeyboardMarkup(button)
