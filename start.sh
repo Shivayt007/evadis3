6816ab84756aace7a8bc71ce2c5f50762e73757f
Race Developer Members <88087547+Shivayt007@users.noreply.github.com>
2023-07-08 07:42:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git 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@github.com/shivayt007/evadis2.git /DQTheFileDonorBot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
-fi
+
 cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
