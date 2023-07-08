c149b359640eda962e67435e748ea80a90656d07
Race Developer Members <88087547+Shivayt007@users.noreply.github.com>
2023-07-08 07:35:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone -b main https://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@github.com/shivayt007/evadis2.git /DQTheFileDonorBot
+  git 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@github.com/shivayt007/evadis2.git /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQTheFileDonorBot
